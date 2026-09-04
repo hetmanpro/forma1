627561516b171e33e40fd80fe4f52b3411c7ef06
hetmanpro <hetmanpro@gmail.com>
2026-09-04 14:53:49 +0200

    Update Instagram link to https://www.instagram.com/forma_vienna

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,9 @@
 <?php
-// TODO: confirm real Instagram handle with the client before launch.
 return [
     'whatsapp_number' => '+43 676 9242326',
     'whatsapp_e164'   => '436769242326',
     'contact_email'   => 'forma.vienna@gmail.com',
     'address'         => "Nordbahnstrasse 15, 1020 Vienna",
     'maps_url'        => 'https://maps.app.goo.gl/3ygLfBX9yFgHFbu99',
-    'instagram_url'   => 'https://instagram.com/forma', // TODO: real handle
+    'instagram_url'   => 'https://www.instagram.com/forma_vienna',
 ];
